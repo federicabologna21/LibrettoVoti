3ff2833623f506cd694fa0f122defbed91140735
utente <utente@LAPTOP-L0TNJ455.station>
2021-03-08 11:23:01 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -25,4 +25,47 @@ public class Libretto {
 		}
 		return s;
 	}
+	
+	// Richiesta 2
+	
+	/*
+	public void stampaVotiUguali (int punteggio) {
+		// il libretto stampa da solo i voti
+		System.out.println(); // fa solo questo l'altro metodo è più versatile
+	}
+	
+	//oppure 
+	// metodo più generale
+	public String votiUguali (int punteggio) {
+		// calcola una stringa che contiene i voti 
+		// sarà poi il chiamante a stamparli
+		// -> Solo il NOME?
+		// -> Tutto il voto .toString()
+		// perche il punto 2 è ambiguo 
+		
+	}
+	*/
+	
+	public List<Voto> listaVotiUguali(int punteggio){
+		// restituisce solo i voti uguali al criterio 
+		List<Voto> risultato = new ArrayList<>();
+		for (Voto v: this.voti) {
+			if (v.getVoto()== punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
+
+	public Libretto votiUguali (int punteggio) {
+		Libretto risultato = new Libretto();
+		for (Voto v: this.voti) {
+			if (v.getVoto() == punteggio) {
+				risultato.add(v);
+				// risultato.voti.add(v); giusto ma superfluo 
+				}
+		}
+		return risultato;
+	} 
+	
 }
--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.librettovoti.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 
@@ -16,9 +17,16 @@ public class TestLibretto {
 		libretto.add(voto1);
 		libretto.add(new Voto("Fisica 1", 28, LocalDate.of(2019, 7, 15)));
 		libretto.add(new Voto("Informatica", 24, LocalDate.of(2019, 9, 15)));
+		libretto.add(new Voto("Chimica", 25, LocalDate.of(2019, 9, 15)));
 		// ne mettiamo 3 ma sarebbero 10
 		
 		System.out.println(libretto);  // cosi stampa il riferimento all'oggetto ma non l'oggetto
 		// nella classe Libretto creo il metodo toString
+		
+		List<Voto> venticinque = libretto.listaVotiUguali(25);
+		System.out.println(venticinque);
+		
+		Libretto librettoventicinque = libretto.votiUguali(25);
+		System.out.println(librettoventicinque);
 	}
 }
